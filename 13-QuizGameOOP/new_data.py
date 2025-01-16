f6f1fc2d448360c3cf0576775b74702401a8c10f
Jiguita Kumar <64022038+JiguitaKumar@users.noreply.github.com>
2025-01-16 10:24:44 +0100

    Create new_data.py

---
+++ 13-QuizGameOOP/new_data.py
@@ -0,0 +1,82 @@
+new_question_data = [
+    {
+        "type":"boolean",
+        "difficulty":"easy",
+        "category":"Entertainment: Television",
+        "question":"The Simpsons' family is named after creator Matt Groening's real family.",
+        "correct_answer":"True",
+        "incorrect_answers":["False"]
+    },
+    {
+        "type":"boolean",
+        "difficulty":"medium",
+        "category":"Entertainment: Television",
+        "question":"Like his character in Parks and Recreation, Aziz Ansari was born in South Carolina.",
+        "correct_answer":"True",
+        "incorrect_answers":["False"]
+    },
+    {
+        "type":"boolean",
+        "difficulty":"easy",
+        "category":"Entertainment: Television",
+        "question":"Star Trek, Klaang is a typical Klingon male.",
+        "correct_answer":"True",
+        "incorrect_answers":["False"]
+    },
+    {
+        "type":"boolean",
+        "difficulty":"medium",
+        "category":"Entertainment: Television",
+        "question":"In Star Trek, Klingons respect William Shakespeare, they even suspect him having a Klingon lineage.",
+        "correct_answer":"True",
+        "incorrect_answers":["False"]
+    },
+    {
+        "type":"boolean",
+        "difficulty":"medium",
+        "category":"Entertainment: Television",
+        "question":"Klingons respect their disabled comrades, and those who are old, injuried, and helpless.",
+        "correct_answer":"False",
+        "incorrect_answers":["True"]
+    },
+    {
+        "type":"boolean",
+        "difficulty":"hard",
+        "category":"Entertainment: Television",
+        "question":"The Klingon home planet is QonoS.",
+        "correct_answer":"True",
+        "incorrect_answers":["False"]
+    },
+    {
+        "type":"boolean",
+        "difficulty":"easy",
+        "category":"Entertainment: Television",
+        "question":"In Doctor Who, the Doctor gets his TARDIS by stealing it.",
+        "correct_answer":"True",
+        "incorrect_answers":["False"]
+    },
+    {
+        "type":"boolean",
+        "difficulty":"easy",
+        "category":"Entertainment: Television",
+        "question":"In Star Trek, Klingons are aliens.",
+        "correct_answer":"True",
+        "incorrect_answers":["False"]
+    },
+    {
+        "type":"boolean",
+        "difficulty":"easy",
+        "category":"Entertainment: Television",
+        "question":"In Battlestar Galactica (2004), Cylons were created by man as cybernetic workers and soldiers.",
+        "correct_answer":"True",
+        "incorrect_answers":["False"]
+    },
+    {
+        "type":"boolean",
+        "difficulty":"medium",
+        "category":"Entertainment: Television",
+        "question":"Bob Ross was a US Air Force pilot.",
+        "correct_answer":"False",
+        "incorrect_answers":["True"]
+    }
+]
