542a6eeb21b3c4f79fcde8800dd46c059051d575
Jiguita Kumar <64022038+JiguitaKumar@users.noreply.github.com>
2025-03-17 19:39:39 +0000

    Update scoreboard.py

--- 17-PongGame/scoreboard.py
+++ 17-PongGame/scoreboard.py
@@ -1,11 +1,20 @@
 from turtle import Turtle
 
+
 class Scoreboard(Turtle):
 
-    def __init__(self, score, y_coord):
+    def __init__(self, left_score, right_score):
         super().__init__()
+        self.score_update(left_score, right_score)
+
+    def default_setup(self, score, y_coord):
         self.ht()
         self.penup()
         self.color("white")
         self.goto(y_coord, 250)
         self.write(score, False, "center", ("Trebuchet MS", 30, "bold"))
+
+    def score_update(self, left_score, right_score):
+        self.clear()
+        self.default_setup(right_score, 30)
+        self.default_setup(left_score, -30)
