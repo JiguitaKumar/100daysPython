863bf8878e84d32a7e520f210e98e3ff8fa6e105
Jiguita Kumar <64022038+JiguitaKumar@users.noreply.github.com>
2025-05-29 16:56:05 +0100

    Update main.tf

--- 21-PhoneticAlphabet/main.tf
+++ 21-PhoneticAlphabet/main.tf
@@ -5,6 +5,13 @@ df_letters = pandas.read_csv("nato_phonetic_alphabet.csv")
 letter_code_dict = {row.letter:row.code for (index, row) in df_letters.iterrows()}
 
 # Create a list of the phonetic code words from a word that the user inputs.
-user_input = input("Enter a word: ")
-phonetic_code_list = [letter_code_dict[letter.upper()] for letter in user_input]
-print(phonetic_code_list)
+get_input = True
+while get_input:
+    user_input = input("Enter a word: ")
+    try:
+        phonetic_code_list = [letter_code_dict[letter.upper()] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+    else:
+        print(phonetic_code_list)
+        get_input = False
